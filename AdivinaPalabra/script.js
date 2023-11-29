09b7ff563dcd56108cdc18880bd8c1f90bcd656f
fdariasglez@hotmail.com <fdariasglez@hotmail.com>
2023-11-29 11:19:13 +0000

    ADD Juego Advina

--- AdivinaPalabra/script.js
+++ AdivinaPalabra/script.js
@@ -1,10 +1,12 @@
-let wordList = ["javascript", "programming", "developer", "html", "css", "challenge"];
+var wordList = ["none"];
 let randomWord;
 let guessedWord;
 let failedAttempts = 0;
-let maxAttempts = 6;
+let maxAttempts = 15;
+let gameFinished = false;
 
-function initializeGame() {
+async function initializeGame() {
+  await loadWordList('https://raw.githubusercontent.com/webpwnized/byepass/master/dictionaries/top-10000-spanish-words.txt');
   randomWord = getRandomWord();
   guessedWord = Array(randomWord.length).fill('_');
   displayWord();
@@ -12,6 +14,22 @@ function initializeGame() {
   updateAttempts();
   document.getElementById('result').innerHTML = '';
   setupLetterButtons();
+  gameFinished = false;
+}
+
+function loadWordList(url) {
+  // fetch url (txt) and each line will be a word, skip the word is have some letter isn't on the abecedary
+  return fetch(url)
+    .then(response => response.text())
+    .then(data => {
+      wordList = data.split('\n').filter(word => {
+        return word.split('').every(letter => {
+          return 'abcdefghijklmnñopqrstuvwxyz'.includes(letter);
+        });
+      });
+    }).catch(err => {
+      console.error(err);
+    });
 }
 
 function getRandomWord() {
@@ -24,22 +42,35 @@ function displayWord() {
 }
 
 function setupLetterButtons() {
-  const letterButtonsContainer = document.getElementById('letter-buttons');
-  letterButtonsContainer.innerHTML = ''; // Limpiar los botones existentes
+  const letterTable = document.getElementById('letter-table');
+  letterTable.innerHTML = ''; // Limpiar la tabla
 
-  for (let i = 0; i < 26; i++) {
-    const letter = String.fromCharCode(97 + i); // Convertir el código ASCII a letra minúscula
-    const button = document.createElement('button');
-    button.textContent = letter;
-    button.onclick = function() {
-      checkGuess(letter);
-      button.disabled = true; // Desactivar el botón después de hacer clic
-    };
-    letterButtonsContainer.appendChild(button);
+  const alphabet = 'abcdefghijklmnñopqrstuvwxyz';
+  let index = 0;
+
+  for (let i = 0; i < 3; i++) {
+    const row = letterTable.insertRow(i);
+    for (let j = 0; j < 9; j++) {
+      const cell = row.insertCell(j);
+      const letter = alphabet[index];
+      const button = document.createElement('button');
+      button.textContent = letter.toUpperCase();
+      button.onclick = function () {
+        checkGuess(letter);
+      };
+      button.id = `btn-${letter}`;
+      cell.appendChild(button);
+      index++;
+    }
   }
 }
 
 function checkGuess(guess) {
+  if (gameFinished) return;
+
+  const button = document.getElementById(`btn-${guess}`);
+  button.disabled = true;
+
   if (randomWord.includes(guess)) {
     for (let i = 0; i < randomWord.length; i++) {
       if (randomWord[i] === guess) {
@@ -47,20 +78,32 @@ function checkGuess(guess) {
       }
     }
     displayWord();
+    button.style.backgroundColor = "#2ecc71"; // Verde pastel
   } else {
     failedAttempts++;
     updateAttempts();
+    button.style.backgroundColor = "#e74c3c"; // Rojo
   }
 
   if (guessedWord.join('') === randomWord) {
     document.getElementById('result').innerHTML = '¡Felicidades! ¡Adivinaste la palabra!';
+    endGame();
   } else if (failedAttempts === maxAttempts) {
     document.getElementById('result').innerHTML = `¡Agotaste tus intentos! La palabra era "${randomWord}". ¡Mejor suerte la próxima vez!`;
+    endGame();
   }
 }
 
 function updateAttempts() {
   document.getElementById('attempts').innerHTML = `Fallos restantes: ${maxAttempts - failedAttempts}`;
+}
+
+function endGame() {
+  gameFinished = true;
+  const letterButtons = document.querySelectorAll('button[id^="btn-"]');
+  letterButtons.forEach(button => {
+    button.disabled = true;
+  });
 }
 
 function resetGame() {
